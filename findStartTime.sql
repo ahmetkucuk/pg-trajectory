383869c0cec3e46bfa2081717f0ad58a3e82221b
berkay_aydin <berqi89@gmail.com>
2016-03-04 12:12:31 +0000

    findStartTime.sql created online with Bitbucket

---
+++ findStartTime.sql
@@ -0,0 +1,33 @@
+DROP FUNCTION IF EXISTS findStartTime(integer);
+--findStartTime::finds the start time of a particular trajectory identified by tr_id
+--@param tr_id::integer that represents the trajectory identifier
+CREATE OR REPLACE FUNCTION findStartTime(tr_id integer) RETURNS timestamp AS 
+$BODY$
+DECLARE
+    tgp tg_pair;
+    tgpairs tg_pair[];
+    startTime timestamp;
+BEGIN
+
+    select tr_data into tgpairs
+    from test_tr
+    where id = tr_id;
+    if not found then
+	raise exception 'trajectory id % not found', 2;
+    end if;
+    --TODO check null condition here
+
+    --RAISE NOTICE 'my timestamp --> %', tgpairs[1].t;
+    startTime := tgpairs[2].t;
+
+    FOREACH tgp IN ARRAY tgpairs
+    LOOP
+        --RAISE NOTICE 'loop timestamp --> %', tgp.t;
+    	IF startTime > tgp.t THEN
+	    startTime := tgp.t;
+	END IF;
+    END LOOP;
+    RETURN startTime;
+END
+$BODY$
+LANGUAGE 'plpgsql' ;
