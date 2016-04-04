1f6eff09235cc522c8c0dff7c30bf59bd1893b07
hamdi <tohamdi@gmail.com>
2016-04-03 22:47:03 -0400

    demo queries updated

--- test/DEMO/DemoQueries.sql
+++ test/DEMO/DemoQueries.sql
@@ -36,7 +36,26 @@ SELECT tg_edit_distance_2(t1.tr, t2.tr, '1'::NUMERIC, TRUE), (t1.tr).geom_type, 
 FROM trajectory_table t1, trajectory_table t2
 WHERE (t1.tr).geom_type = 'Polygon' AND (t2.tr).geom_type = 'Polygon' LIMIT 1;
 
+--zNormalize
+SELECT zNormalize((t.tr).tr_data,2,3) FROM trajectory_table t WHERE t.id = 2;
 
+--Euclidean Distance for points
+SELECT t1.id AS traj_1_ID, t2.id AS traj_2_ID, EuclideanDistance(t1.tr, t2.tr) AS EuclideanDistance
+FROM trajectory_table t1, trajectory_table t2
+WHERE (t1.tr).geom_type = 'Point' AND (t2.tr).geom_type = 'Point';
+
+--Modifying the trajecotries; trajectory 2 (point) spans from time 10 to 30
+UPDATE trajectory_table t 
+SET tr = drop_head(tr)
+WHERE id = 1;
+
+UPDATE trajectory_table t
+SET tr = add_at_tail(( to_timestamp(40), ST_GeomFromText('POINT(9 10)') )::tg_pair, tr)
+WHERE id = 1;
+
+UPDATE trajectory_table t
+SET tr = modify_at_time( to_timestamp(30), ST_GeomFromText('POINT(500 600)'), tr)
+WHERE id = 1;
 
 --APPENDIX
 SELECT t1.id, t2.id, t_area(t1.tr) as area1, t_area(t2.tr) as area2, t_omax(t1.tr, t2.tr) as omax
